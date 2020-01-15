ccc1524488f43d4e846a8fcddbbef26da6b7dfb0
tomafoks <tomafoks@mail.ru>
2020-01-16 02:09:16 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.addEventListener("DOMContentLoaded", function() {
+    "use strict";
+
+    let info = document.querySelector('.info-header'),
+        tab = document.querySelectorAll('.info-header-tab'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+   
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+    
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        if (event.target && event.target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (event.target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+});
