174b86ab5aa9fa7c6dcc551a3b827a57b997be6a
tomafoks <tomafoks@mail.ru>
2020-01-18 13:17:32 +0500

    add timer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 window.addEventListener("DOMContentLoaded", function() {
     "use strict";
 
+    // tabs:
+
     let info = document.querySelector('.info-header'),
         tab = document.querySelectorAll('.info-header-tab'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -32,4 +34,49 @@ window.addEventListener("DOMContentLoaded", function() {
             }
         }
     });
+
+    // timer:
+let deadLine = '2025-02-21';
+
+function getTimeRemaining(endTime) {
+    let t = Date.parse(endTime) - Date.parse(new Date()),
+        seconds = Math.floor((t / 1000) % 60),
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+function setClock(id, endTime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+
+        timeInerval = setInterval(updateClock, 1000);
+    
+    function updateClock() {
+        let t = getTimeRemaining(endTime);
+        for (let key in t) {
+            if (t[key].toString().length == 1) {
+                t[key] = '0' + t[key];
+            }
+        }
+        hours.textContent = t.hours;
+        minutes.textContent = t.minutes;
+        seconds.textContent = t.seconds;
+
+        if (t.total <= 0) {
+            clearInterval(timeInerval);
+        }
+    }
+}
+
+setClock("timer", deadLine);
+
 });
